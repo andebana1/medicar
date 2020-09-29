a6022abd69541c93abd51316bc290b3e8d771b04
andebana1 <andersonvieira14@gmail.com>
2020-09-29 07:10:22 -0300

    melhorias na tela de criar usuário

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { UserService } from '../../shared/user.service';
@@ -30,7 +30,8 @@ export class RegisterComponent implements OnInit {
       username: ['', Validators.required],
       email: ['', [Validators.email, Validators.required]],
       password1: ['', Validators.required],
-      password2: ['', Validators.required]
+      password2: ['', Validators.required],
+      savePass: new FormControl(false),
     });
   }
 }
